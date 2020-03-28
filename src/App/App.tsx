38434dcbf1b6ea0c70a5ae3a626e398a529d2c80
apbs <aparnabs.prasad@gmail.com>
2020-03-28 19:30:03 +0100

    init

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NumberCard from '../NumberCard/NumberCard';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NumberCard/NumberCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NumberCard = () => {
+    return <div>Number Card</div>
+}
+
+export default NumberCard;
